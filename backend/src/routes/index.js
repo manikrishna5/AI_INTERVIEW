1a4cc80c52abb63badb9a8681bdef75f309f7db7
Mani Krishna Karri <manikrishnakarri@gmail.com>
2026-06-26 15:28:55 +0530

    Auth Test

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 
 import healthRoutes from "./health.routes.js";
+import authRoutes from "./auth.routes.js";
 
 const router = express.Router();
 
 router.use("/", healthRoutes);
+router.use("/auth", authRoutes);
+
 
 export default router;
