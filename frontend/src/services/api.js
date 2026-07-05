e127d8a6bf7bb462e562a8831da94ed8793f18c5
Mani Krishna Karri <manikrishnakarri@gmail.com>
2026-07-05 21:01:34 +0530

    Frontend Done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-     import.meta.env.VITE_API_URL ||
-     "http://localhost:5000",
+    import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
